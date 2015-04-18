e81ff1d948378e21f150c99870c8a0acce4d4f81
Jomar Tigcal <jomar@tigcal.com>
2015-04-18 19:38:02 +0800

    Sending help SMS from wearable device
    Signed-off-by: Jomar Tigcal <jomar@tigcal.com>

---
+++ mobile/src/main/java/com/tigcal/helpme/DataLayerListener.java
@@ -0,0 +1,21 @@
+package com.tigcal.helpme;
+
+import android.content.Intent;
+
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class DataLayerListener extends WearableListenerService {
+    private static final String WEARABLE_MESSAGE = "/start/MainActivity";
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        super.onMessageReceived(messageEvent);
+        if(WEARABLE_MESSAGE.equals(messageEvent.getPath())) {
+            Intent intent = new Intent(this, SendSmsService.class);
+            intent.putExtra(SendSmsService.MOBILE_NUMBER, "//DUMMY");
+
+            startService(intent);
+        }
+    }
+}
--- wear/src/main/java/com/tigcal/helpme/HelpMeWearActivity.java
+++ wear/src/main/java/com/tigcal/helpme/HelpMeWearActivity.java
@@ -3,11 +3,25 @@ package com.tigcal.helpme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-public class HelpMeWearActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.List;
+
+public class HelpMeWearActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = "HelpMeWearActivity";
+    private static final String HANDHELD_PATH = "/start/MainActivity";
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +41,55 @@ public class HelpMeWearActivity extends Activity {
 
             }
         });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGoogleApiClient.disconnect();
     }
 
     private void askForHelp() {
-        Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
+        if(mGoogleApiClient == null) {
+            return;
+        }
+
+        final PendingResult<NodeApi.GetConnectedNodesResult> nodes = Wearable.NodeApi.getConnectedNodes(mGoogleApiClient);
+        nodes.setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
+            @Override
+            public void onResult(NodeApi.GetConnectedNodesResult connectedNodesResult) {
+                final List<Node> nodes = connectedNodesResult.getNodes();
+                for (int i = 0; i < nodes.size(); i++) {
+                    Wearable.MessageApi.sendMessage(mGoogleApiClient, nodes.get(i).getId(), HANDHELD_PATH, null);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "onConnected(): Successfully connected to Google API client");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended(): Connection to Google API client was suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "onConnectionFailed(): Failed to connect, with result: " + connectionResult);
     }
 }
