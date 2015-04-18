8a800c3508773d9c340a0d7dc4363c64f3e1e800
Jomar Tigcal <jomar@tigcal.com>
2015-04-18 11:02:10 +0800

    Getting and saving contact number
    Signed-off-by: Jomar Tigcal <jomar@tigcal.com>

--- mobile/src/main/java/com/tigcal/helpme/MainActivity.java
+++ mobile/src/main/java/com/tigcal/helpme/MainActivity.java
@@ -1,39 +1,86 @@
 package com.tigcal.helpme;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String CONTACT_NUMBER = "contact_mobile_number";
+
+    private SharedPreferences mPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPreferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
+
+        ImageView askHelpImage = (ImageView) findViewById(R.id.image_ask_help);
+        askHelpImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                askForHelp();
+            }
+        });
+
+        final EditText contactNumberText = (EditText) findViewById(R.id.text_contact_number);
+        contactNumberText.setText(mPreferences.getString(CONTACT_NUMBER, ""));
+
+        Button saveContactNumber = (Button) findViewById(R.id.button_save);
+        saveContactNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveContactNumber(contactNumberText.getText().toString());
+            }
+        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_about) {
             new AboutDialog(this).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void saveContactNumber(String contactNumber) {
+        if (contactNumber == null || "".equals(contactNumber)) {
+            displayInvalidNumberMessage();
+            return;
+        }
+
+        mPreferences.edit()
+                .putString("contact_mobile_number", contactNumber)
+                .commit();
+
+    }
+
+    private void displayInvalidNumberMessage() {
+        Toast.makeText(this, getString(R.string.message_invalid_contact_number), Toast.LENGTH_SHORT).show();
+    }
+
+    private void askForHelp() {
+        //TODO
     }
 }
